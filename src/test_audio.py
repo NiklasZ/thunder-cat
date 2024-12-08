adb1b5de173eed3e6ef515db55ab16e6e1692bde
Andreea Luțac <andreea@auterion.com>
2024-12-08 16:37:24 +0100

    feat: add playback lock

--- src/test_audio.py
+++ src/test_audio.py
@@ -1,5 +1,10 @@
 # Play audio on a specific device by ID (e.g., device 1)
-from src.libaudio import play
+from libaudio import play, is_playing
+import time
 
-file_path = "/home/rae-rae/killing.wav"
-play(file_path, device_id=3)
+file_path = "/home/rae-rae/thunder-cat/sound/zoidberg.wav"
+while True:
+    if not is_playing():
+        print("Attempting to play.")
+        play(file_path, device_id=3)
+    time.sleep(2)
